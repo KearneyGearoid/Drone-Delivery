ff9b669fdba1f7822eed91b66ddc0db83eac9a08
Aoife Marie Horgan <aoifehxo@gmail.com>
2016-12-02 15:26:48 +0000

    Type Safety, Encapsulation, added extra attribute numDeliveries

--- DroneDeliveryAoife/src/FindGroupDeliveryTester.java
+++ DroneDeliveryAoife/src/FindGroupDeliveryTester.java
@@ -1,3 +1,7 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by Aoife Horgan on 02/11/2016.
  */
@@ -7,20 +11,20 @@ public class FindGroupDeliveryTester{
 
         TrackGroupDelivery delivery = new TrackGroupDelivery();
         //initializeTrackGroupDelivery(delivery);
-        Liist<Double> weights = new LinkedList<>();
+        List<Double> weights = new LinkedList<>();
         weights.add(40.27);
         weights.add(20.87);
         List<Double> dimensions = new LinkedList<>();
         dimensions.add(15.6);
         dimensions.add(10.0);
 
-        GroupDelivery deliverySample = new GroupDelivery(1,"Sample Delivery","Tralee" ,"Killarney" ,weights,dimensions,40.00);
+        GroupDelivery deliverySample = new GroupDelivery(1,"Sample Delivery","Tralee" ,"Killarney" ,weights,dimensions,40.00,1);
 
 
         List deliveryMatching = delivery.search(deliverySample);
         if(!deliveryMatching.isEmpty()){
             System.out.println("List of deliveries");
-            for(Iterator i = deliveryMatching.iterator();i.hasNext();){
+            for(Iterator i = deliveryMatching.iterator(); i.hasNext();){
                 GroupDelivery del = (GroupDelivery)i.next();
                 System.out.println("Delivery Information: " + "\nDelivery Id: " + del.getDeliveryId() + "\nDelivery Name: " +
                 del.getDeliveryName() + "\nDelivery Cost: " + del.getDeliveryCost());
