678d3f827aafc65ef84eed2266d4cbc705e011d1
Conor Hall <conorhall.96@gmail.com>
2016-12-05 13:37:36 +0000

    Changed LinkedList to List
    Chnaged the declaration of the LinkedList of drones to a list, this is
    to fix concrete dependency.

--- DroneDeliveryConor/src/DroneFleet.java
+++ DroneDeliveryConor/src/DroneFleet.java
@@ -1,12 +1,13 @@
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by Conor on 23/11/2016.
  */
 public class DroneFleet {
 
-    public LinkedList<Drone> drones = new LinkedList<>();
+    public List<Drone> drones = new LinkedList<>();
 
     public  Drone searchDrone(Drone drone){
         for(Iterator i = drones.iterator(); i.hasNext();){
