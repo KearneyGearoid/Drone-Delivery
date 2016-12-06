71a82d8680dc1d54c03a54523b648c80318db663
Conor Hall <conorhall.96@gmail.com>
2016-12-06 19:20:58 +0000

    Created Test Classes for DroneFleet
    Created JUnit tests for the DroneFleet methods in DroneFleetTest.java

---
+++ DroneDeliveryConor/test/DroneFleetTest.java
@@ -0,0 +1,44 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Conor on 06/12/2016.
+ */
+public class DroneFleetTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void searchDrone() throws Exception {
+        System.out.println("addDrone()");
+
+        DroneFleet instance = new DroneFleet();
+    }
+
+    @org.junit.Test
+    public void addDrone() throws Exception {
+        System.out.println("addDrone()");
+    }
+
+    @org.junit.Test
+    public void getDrone() throws Exception {
+        System.out.println("getDrone()");
+    }
+
+    @org.junit.Test
+    public void removeDrone() throws Exception {
+        System.out.println("addRemoveDrone()");
+    }
+
+    @org.junit.Test
+    public void sendStatusRequest() throws Exception {
+        System.out.println("sendStatusRequest()");
+    }
+
+}
