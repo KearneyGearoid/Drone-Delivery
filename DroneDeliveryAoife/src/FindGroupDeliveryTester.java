380b448cf5a2a36ac0537633cab99c49c6d03684
Aoife Marie Horgan <aoifehxo@gmail.com>
2016-12-08 14:44:28 +0000

    Added Group Deliverys
    Added Group Deliverys to the Inventory class TrackGroupDelivery to
    populate the database with deliveries so the user is able to search a
    group delivery

--- DroneDeliveryAoife/src/FindGroupDeliveryTester.java
+++ DroneDeliveryAoife/src/FindGroupDeliveryTester.java
@@ -7,14 +7,18 @@ import java.util.List;
  */
 
 public class FindGroupDeliveryTester{
+
+    public static List<Double> weights;
+    public static List<Double> dimensions;
     public static void main(String[] args){
 
         TrackGroupDelivery delivery = new TrackGroupDelivery();
         //initializeTrackGroupDelivery(delivery);
-        List<Double> weights = new LinkedList<>();
+        weights = new LinkedList<>();
+        dimensions = new LinkedList<>();
         weights.add(40.27);
         weights.add(20.87);
-        List<Double> dimensions = new LinkedList<>();
+
         dimensions.add(15.6);
         dimensions.add(10.0);
 
@@ -31,6 +35,14 @@ public class FindGroupDeliveryTester{
             }
         }else
             System.out.println("No deliveries");
+
+    }
+    private static void initializeInventory(TrackGroupDelivery inventory) {
+        inventory.addGroupDelivery(2,"Sample Delivery","Tralee" ,"Killarney" ,weights,dimensions,40.00,1);
+        inventory.addGroupDelivery(2,"Sample Delivery","Killarney" ,"Tralee" ,weights,dimensions,40.00,2);
+        inventory.addGroupDelivery(3,"Sample Delivery","Cork" ,"Tralee" ,weights,dimensions,40.00,2);
+        inventory.addGroupDelivery(4,"Sample Delivery","Mayo" ,"Tralee" ,weights,dimensions,40.00,1);
+        inventory.addGroupDelivery(5,"Sample Delivery","Dublin" ,"Tralee" ,weights,dimensions,40.00,2);
 
     }
 }
