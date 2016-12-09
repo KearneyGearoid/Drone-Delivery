90ac3385f15740260eb9045c180c3c18130d1742
Gearoid Kearney <gearoid.kearney@me.com>
2016-12-09 20:22:06 +0000

    Changed LinkedList to List
    Changed this due to the principle of coding to an interface not implementation. This allows the program to change how
    it uses List interface , whether the program wants to use a LinkedList and ArrayList.

--- DroneDeliveryGearoid/src/TrackDelivery.java
+++ DroneDeliveryGearoid/src/TrackDelivery.java
@@ -1,18 +1,19 @@
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by Gearoid on 27/11/2016.
  */
 public class TrackDelivery {
 
-    static LinkedList<Delivery> deliveries = new LinkedList<>();
+    static List<Delivery> deliveries = new LinkedList<>();
 
     public static String searchDeliveries(int DeliveryId){
         for (Delivery element : deliveries) {
 
             if (DeliveryId == element.getDeliveryId()) {
                 return "Delivery Id: " + element.getDeliveryId() + "\nDelivery Location: " + element.getDeliveryLocation() + "\nCollection Location: " + element.getCollectionLocation()
-                        + "Weight" + element.getWeight();
+                        + "\nWeight" + element.getWeight();
             }
 
         }
